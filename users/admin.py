a27eea93d9936cf118d1b5339a3fbc559dc19d66
Michael Vitollo <ace51689@gmail.com>
2021-10-06 12:04:46 -0400

    Created a templates folder with signup.html, and users.forms.py. Created a CreateUserForm inside forms.py and added a class RedditUserAdmin inside users.admin.py. Created a class based view to handle user creation on the front end and displaying the view at url '/signup/'.

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from users.models import User
+from users.forms import CreateUserForm
 
-admin.site.register(User)
+class RedditUserAdmin(UserAdmin):
+  model = User
+  add_form = CreateUserForm
+
+admin.site.register(User, RedditUserAdmin)
