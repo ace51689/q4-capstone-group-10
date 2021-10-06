83e3af9a42bfbb60ef9708c6f41140a8e3839153
Cassandra Johnson <cassandra.johnson83@outlook.com>
2021-10-06 13:01:20 -0600

    starting point

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+
 
 from users import views as user_views
+from users import views
 
 urlpatterns = [
-    path('signup/', user_views.CreateUserView.as_view(), name='signup'),
+    path('', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
