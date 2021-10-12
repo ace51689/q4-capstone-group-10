90eee42a5b6f71b97489dfe0da0ecef6795cddd7
Michael Vitollo <ace51689@gmail.com>
2021-10-11 21:39:53 -0400

    Added forms and urls inside posts directory, and create_post.html and post.html to the templates directory. Users now have the ability to create posts from subreddits they are subscribed to.

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from django.views.generic import View
+from posts.models import Post
+from subreddits.models import Subreddit
+from posts.forms import CreatePostForm, CreateCommentForm
 
 # Create your views here.
+def post_view(request, id):
+  post = Post.objects.get(id=id)
+  return render(request, 'post.html', { 'post': post })
+
+
+def create_post_view(request, id):
+  subreddit = Subreddit.objects.get(id=id)
+
+  if request.method == 'POST':
+    form = CreatePostForm(request.POST)
+    if form.is_valid():
+      data = form.cleaned_data
+      post = Post(
+        title = data.get('title'),
+        body = data.get('body'),
+        is_comment = False,
+        author = request.user,
+        subreddit = subreddit,
+      )
+      post.save()
+      return HttpResponseRedirect(reverse('post', args=(post.id,)))
+
+  form = CreatePostForm()
+
+  return render(request, 'create_post.html', { 'form': form })
