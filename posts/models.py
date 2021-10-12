74e3bc07c0051ffdfce256ce1b2da8fe3f729275
Lorenzo Ginyard <lnginyard@gmail.kenzie.academy.com>
2021-10-12 10:03:47 -0400

    Home page Fixes #4

---
+++ posts/models.py
@@ -0,0 +1,16 @@
+ from mptt.models import MPTTModel, TreeForeignKey
+from django.db import models
+
+
+class Post(MPTTModel):
+	title = models.CharField(max_length=100, null=True, blank=True)
+	body = models.TextField(null=True, blank=True)
+	is_comment = models.BooleanField(default=False)
+	author = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='user')
+	up_votes = models.ManyToManyField('users.User', symmetrical=False, blank=True, related_name='post_up_votes')
+	down_votes = models.ManyToManyField('users.User', symmetrical=False, blank=True, related_name='post_down_votes')
+	subreddit = models.ForeignKey('subreddits.Subreddit', on_delete=models.CASCADE, null=True, blank=True)
+	parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+	def __str__(self):
+		return self.title or self.body
