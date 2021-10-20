4c1940829bd8ff6308fcfef64ff45912db27872e
Malcomputer <malcolmmkubwa@gmail.com>
2021-10-20 14:23:23 -0500

    add static file root

--- config/settings.py
+++ config/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATICFILES_DIRS = [BASE_DIR/'static']
+STATIC_ROOT = [BASE_DIR, 'staticfiles']
 STATIC_URL = '/static/'
 
 LOGIN_URL = '/login/'
